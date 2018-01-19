837b18569314520847dee5e8bc23e420ec55fbb5
Hamish Coleman <hamish@zot.org>
2018-01-19 17:38:10 +0800

    Create a zfs cache during install

---
+++ zfs-config/packages.d/_ALWAYS.customise.add/zfs.d/11-inchroot/11.15-zfs-cachefile.sh
@@ -0,0 +1,8 @@
+#
+# create a cachefile with our current settings
+#
+# there is a systemd unit to load the cache on bootup, which fails if the
+# file is not there - no other issues have been seen from missing it (but
+# no complex disk setups were used either)
+
+zpool set cachefile=/etc/zfs/zpool.cache $ROOT_POOL
