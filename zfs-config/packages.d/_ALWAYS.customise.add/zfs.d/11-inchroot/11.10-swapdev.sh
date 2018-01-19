25246369fe2bf50dec2963ef99cefd1b5eb914be
Hamish Coleman <hamish@zot.org>
2018-01-19 17:32:49 +0800

    Disable swapfile creation until block device discovery is debugged

--- zfs-config/packages.d/_ALWAYS.customise.add/zfs.d/11-inchroot/11.10-swapdev.sh
+++ zfs-config/packages.d/_ALWAYS.customise.add/zfs.d/11-inchroot/11.10-swapdev.sh
@@ -2,19 +2,19 @@
 # Add a swapdev
 #
 
-zfs create -V 4G -b $(getconf PAGESIZE) \
-    -o compression=zle \
-    -o logbias=throughput \
-    -o sync=always \
-    -o primarycache=metadata \
-    -o secondarycache=none \
-    -o com.sun:auto-snapshot=false \
-    $ROOT_POOL/swap
-
-# FIXME - detect the blockdev name
-mkswap -L swap /dev/zd0
-
-# FIXME - the installed system doesnt see the swap dev label
+#zfs create -V 4G -b $(getconf PAGESIZE) \
+#    -o compression=zle \
+#    -o logbias=throughput \
+#    -o sync=always \
+#    -o primarycache=metadata \
+#    -o secondarycache=none \
+#    -o com.sun:auto-snapshot=false \
+#    $ROOT_POOL/swap
+#
+## FIXME - detect the blockdev name
+#mkswap -L swap /dev/zd0
+#
+## FIXME - the installed system doesnt see the swap dev label
 #echo LABEL=swap none swap defaults 0 0 >> /etc/fstab
 #swapon -av
 #swapoff /dev/zd0
