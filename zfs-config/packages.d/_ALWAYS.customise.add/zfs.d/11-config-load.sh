cbdc9f6523262af0059bca70db64c783497aa639
Hamish Coleman <hamish@zot.org>
2018-02-26 17:36:50 +0800

    Add some placeholders for save/load config steps

---
+++ zfs-config/packages.d/_ALWAYS.customise.add/zfs.d/11-config-load.sh
@@ -0,0 +1,7 @@
+#
+# Load configuration from external files
+#
+
+# TODO
+# - if the boot media has some config, import it
+#   (should take advantage of the /conf.d processing in the initramfs)
---
+++ zfs-config/packages.d/_ALWAYS.customise.add/zfs.d/29-config-save.sh
@@ -0,0 +1,7 @@
+#
+# Save the current config out
+#
+
+# TODO
+# set |grep ^CONFIG_ >/some/file/that/gets/loaded
+
