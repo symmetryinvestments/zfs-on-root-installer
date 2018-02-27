3a5bd9e9dc299f476d370e7dea1cb86274cf6224
Hamish Coleman <hamish@zot.org>
2018-02-27 17:59:12 +0800

    Add fix to disable wayland

---
+++ zfs-config/packages.d/_ALWAYS.customise.add/zfs.d/inchroot/30-fixup-wayland.sh
@@ -0,0 +1,21 @@
+#
+# Install the desktop packages
+#
+
+# check if this is to be an interactive system
+if [ -n "$CONFIG_DESKTOP" ]; then
+    # Ubuntu 17.10 defaults to wayland.  Wayland defaults to suck.  Suck can
+    # be removed from all login managers with the following:
+    mkdir -p /usr/share/wayland-sessions/hidden
+    for i in gnome ubuntu; do
+        dpkg-divert --rename \
+          --divert /usr/share/wayland-sessions/hidden/${i}.desktop \
+          --add /usr/share/wayland-sessions/${i}.desktop
+    done
+    # TODO
+    # - this assumes that only two .desktop type files will ever exist
+
+    # Additionally, to stop gdm3 from using wayland for the login screen:
+    sed -i -E -e 's/^#(WaylandEnable=false)/\1/' /etc/gdm3/custom.conf
+
+fi
