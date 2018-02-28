a3354a2de647342096d491743aed7acf3578ebae
Hamish Coleman <hamish@zot.org>
2018-02-28 17:39:19 +0800

    Add cache and slog detection to disk planner, but no change to installed function yet

--- zfs-config/packages.d/_ALWAYS.customise.add/zfs.d/12-config-disk-plan.sh
+++ zfs-config/packages.d/_ALWAYS.customise.add/zfs.d/12-config-disk-plan.sh
@@ -58,27 +58,97 @@ bdev_best_name() {
     done
 }
 
-# Given a list of block devs, output only the rotational ones
-bdev_only_rotating() {
+DISKDB=/tmp/diskdb
+
+# Given a disk basename and a key/val, save that
+disk_set() {
+    local dev="$1"
+    local key="$2"
+    local val="$3"
+    echo "$val" >"$DISKDB/$dev/$key"
+}
+
+# Given a disk basename and a key return the val
+disk_get() {
+    local dev="$1"
+    local key="$2"
+    local keyfile
+    keyfile="$DISKDB/$dev/$key"
+    if [ -f "$keyfile" ]; then
+        cat "$keyfile"
+    fi
+}
+
+# Given a full pathname for a disk, create a structure for it
+disk__add() {
+    local dev="$1"
+    local bn
+    bn=$(basename "$dev")
+    mkdir -p "$DISKDB/$bn"
+    disk_set "$bn" dev "$dev"
+    disk_set "$bn" size $(lsblk -n -b -d -o SIZE -r "$dev")
+    disk_set "$bn" rota $(lsblk -n -b -d -o ROTA -r "$dev")
+    disk_set "$bn" model $(lsblk -n -b -d -o MODEL -r "$dev")
+}
+
+# Given one or more full pathnames for a disk, create structures for them
+disk_add() {
+    for i in "$@"; do
+        disk__add "$i"
+    done
+}
+
+# Add all the disks in the system to the DISKDB
+disk_add_all() {
+    rm -rf "$DISKDB"
+    disk_add $(bdev_best_name)
+}
+
+# Return a list of all disks in the DISKDB
+disk_all() {
+    for path in $DISKDB/*; do
+        basename $path
+    done
+}
+
+# Given a var/val pair and a list of disks, find the ones that do not match
+disk_find_nomatch() {
+    local var="$1"
+    local val="$2"
+    shift 2
     for dev in "$@"; do
-        if [ "$(lsblk -n -b -d -o ROTA -r "$dev")" == "1" ]; then
-            echo "$dev"
+        if [ "$(disk_get "$dev" "$var")" != "$val" ]; then
+            echo $dev
+        fi
+    done
+}
+
+# Given a var/val pair and a list of disks, find the ones that do match
+disk_find_match() {
+    local var="$1"
+    local val="$2"
+    shift 2
+    for dev in "$@"; do
+        if [ "$(disk_get "$dev" "$var")" = "$val" ]; then
+            echo $dev
         fi
     done
 }
 
 # Naive mirror pairing
-# Given a list of block devs, try to pair them off for mirrorsets
-bdev_to_pairs() {
+# Given a list of disks, try to pair them off for mirrorsets
+disk_to_pairs() {
     prev_size=0
     prev_name=none
     for dev in "$@"; do
-        size=$(lsblk -n -b -d -o SIZE -r "$dev")
+        size=$(disk_get "$dev" size)
 
         # FIXME - should allow 5% slop when comparing
         if [ "$size" == "$prev_size" ]; then
             # TODO - assumes "-part1"
             echo "mirror ${prev_name}-part1 ${dev}-part1"
+            disk_set $(basename "$prev_name") consumed 1
+            disk_set $(basename "$dev") consumed 1
             prev_size=0
             prev_name=none
             continue
@@ -88,35 +158,114 @@ bdev_to_pairs() {
     done
 }
 
-BDEV="$(bdev_best_name)"
+# Look through available disks (in DISKDB) and find something suitable for SLOG
+# possibly finding a pair to mirror
+find_slog() {
+    local disks disks_avail disks_norota model slog
 
-# shellcheck disable=SC2086
-# We actually want to do word splitting on this arg
-ZFS_DISKS="$(bdev_only_rotating $BDEV)"
+    disks_avail=$(disk_find_nomatch consumed 1 $(disk_all))
+    disks_norota=$(disk_find_nomatch rota 1 $disks_avail)
+
+    disks=
+    # first, look for "INTEL SSD" - which we assume has power loss prevention
+    for dev in $disks_norota; do
+        model=$(disk_get "$dev" model)
+        case "$model" in
+            INTEL*) # FIXME
+                disks+=" $dev"
+                ;;
+        esac
+    done
+    slog=$(disk_to_pairs $disks)
+
+    if [ -n "$slog" ]; then
+        echo "log $slog"
+        return 0
+    fi
+
+    # otherwise, just take the first unused SSD
+    for dev in $disks_norota; do
+        case "$dev" in
+            nvme*)
+                continue
+                ;;
+        esac
+        disk_set "$dev" consumed 1
+        echo "log ${dev}-part1"
+        return 0
+    done
+}
+
+# Look through available disks (in DISKDB) and find something fast to use
+# as a cache
+find_cache() {
+    local disks_avail disks_norota
+
+    disks_avail=$(disk_find_nomatch consumed 1 $(disk_all))
+    disks_norota=$(disk_find_nomatch rota 1 $disks_avail)
+
+    # first, look for a NVME - which we assume is "fastest" for reading
+    for dev in $disks_norota; do
+        case "$dev" in
+            nvme*)
+                disk_set "$dev" consumed 1
+                echo "cache ${dev}-part1"
+                return 0
+                ;;
+        esac
+    done
+
+    # otherwise, just take the first unused SSD
+    for dev in $disks_norota; do
+        disk_set "$dev" consumed 1
+        echo "cache ${dev}-part1"
+        return 0
+    done
+}
+
+# initialise the database
+disk_add_all
+
+ZFS_DISKS="$(disk_find_match rota 1 $(disk_all))"
 
 # if there are no rotating disks, just try them all
 if [ -z "$ZFS_DISKS" ]; then
-    ZFS_DISKS="$BDEV"
+    ZFS_DISKS="$(disk_all)"
 fi
 
 # shellcheck disable=SC2086
 # We actually want to do word splitting on this arg
-ZFS_VDEVS="$(bdev_to_pairs $ZFS_DISKS)"
+ZFS_VDEVS="$(disk_to_pairs $ZFS_DISKS)"
 
 # If we could not pair them off
 if [ -z "$ZFS_VDEVS" ]; then
     # some special cases for the expected places we need this
     case "$ZFS_DISKS" in
-        /dev/vda)
-            ZFS_VDEVS=${ZFS_DISKS}1 ;;
-        /dev/disk/by-id/*)
-            ZFS_VDEVS=${ZFS_DISKS}-part1 ;;
+        vda|sda|hda)
+            ZFS_VDEVS=${ZFS_DISKS}1
+            disk_set $(basename "$ZFS_DISKS") consumed 1
+            ;;
+        *)
+            ZFS_VDEVS=${ZFS_DISKS}-part1
+            disk_set $(basename "$ZFS_DISKS") consumed 1
+            ;;
     esac
 fi
+
+ZFS_SLOG="$(find_slog)"
+ZFS_CACHE="$(find_cache)"
 
 echo
 echo "Disks to create full sized data partitions and EFS partitions on:"
 echo "$ZFS_DISKS"
+echo
+# FIXME - add these to the vdev line and add new blah
+echo "Disks to create slog-sized data partitions and use as slog:"
+echo "$ZFS_SLOG"
+echo
+# FIXME - add these to the vdev line and add new blah
+echo "Disks to create cache-sized data partitions and use as cache:"
+echo "$ZFS_CACHE"
 echo
 echo "ZFS Vdevs to create:"
 echo "$ZFS_VDEVS"
