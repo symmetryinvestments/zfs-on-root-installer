c51ce03ab9eac5b978a7b6ccfc9be2a428a23a1b
Hamish Coleman <hamish@zot.org>
2018-03-01 15:49:11 +0800

    Simplify the zfs scrub and add it to the install

--- zfs-config/packages.d/_ALWAYS.customise.add/zfs.d/inchroot/50-zfs-jobs.sh
+++ zfs-config/packages.d/_ALWAYS.customise.add/zfs.d/inchroot/50-zfs-jobs.sh
@@ -2,8 +2,11 @@
 # Suggested ZFS maintenance and support setup
 #
 
+# TODO
+# - these pathnames are hardcoded, it would be nice to use relative paths
+
 # Allow unprivileged users to run read-only ZFS commands
 cp -a /zfs.d/50-zfs.sudoers /etc/sudoers.d/zfs
 
-# FIXME zfs scrub
-
+# A job to scrub the storage pool(s)
+cp -a /zfs.d/50-zfs.scrub /etc/cron.monthly/zfs.scrub
