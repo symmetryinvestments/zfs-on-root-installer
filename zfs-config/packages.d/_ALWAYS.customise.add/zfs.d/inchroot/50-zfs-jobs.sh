b4e194dd7ccae6eba7ea47a341be23abfd14d062
Hamish Coleman <hamish@zot.org>
2018-03-01 15:38:25 +0800

    Allow all users sudo access to read-only ZFS ops
    Add sudoers file from https://gist.githubusercontent.com/fire/65f7aa33b91d3af2aef0/raw/36a2b9e37819abffc6bfc2a9a36859afabf47754/zfs.sudoers

--- zfs-config/packages.d/_ALWAYS.customise.add/zfs.d/inchroot/50-zfs-jobs.sh
+++ zfs-config/packages.d/_ALWAYS.customise.add/zfs.d/inchroot/50-zfs-jobs.sh
@@ -2,6 +2,8 @@
 # Suggested ZFS maintenance and support setup
 #
 
+# Allow unprivileged users to run read-only ZFS commands
+cp -a /zfs.d/50-zfs.sudoers /etc/sudoers.d/zfs
+
 # FIXME zfs scrub
-# FIXME zfs sudoers?
 
