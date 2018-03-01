bc093a924bfd26002d4b94c5087919f1415537c6
Hamish Coleman <hamish@zot.org>
2018-03-01 15:29:34 +0800

    Use a more complete apt sources

--- zfs-config/packages.d/_ALWAYS.customise.add/zfs.d/inchroot/05-sources.sh
+++ zfs-config/packages.d/_ALWAYS.customise.add/zfs.d/inchroot/05-sources.sh
@@ -2,6 +2,22 @@
 # Configure the apt sources list
 #
 
-# FIXME more sources.list
-sed -i -e 's/$/ universe/' /etc/apt/sources.list
+# The sources can be managed with runtime-config-managment after the install
+# is completed, but using a well-defined initial set is probably not a bad
+# idea.
 
+cat > /etc/apt/sources.list << EOF
+deb http://archive.ubuntu.com/ubuntu artful main universe
+deb-src http://archive.ubuntu.com/ubuntu artful main universe
+
+deb http://security.ubuntu.com/ubuntu artful-security main universe
+deb-src http://security.ubuntu.com/ubuntu artful-security main universe
+
+deb http://archive.ubuntu.com/ubuntu artful-updates main universe
+deb-src http://archive.ubuntu.com/ubuntu artful-updates main universe
+EOF
+
+# FIXME
+# - This hardcodes the ubuntu release version of "artful"
+#   We could use the sources list left behind by the debootstrap,
+#   but that doesnt have all the lines needed
