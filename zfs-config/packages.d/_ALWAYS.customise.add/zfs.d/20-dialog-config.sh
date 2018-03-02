eb3df85a5eb01f8eebee695d059451c61f8910c8
Hamish Coleman <hamish@zot.org>
2018-03-02 11:56:50 +0800

    Re-order config prompt so that the most common ones are at the top

--- zfs-config/packages.d/_ALWAYS.customise.add/zfs.d/20-dialog-config.sh
+++ zfs-config/packages.d/_ALWAYS.customise.add/zfs.d/20-dialog-config.sh
@@ -11,15 +11,15 @@ if [ "$CONFIG_UNATTENDED" != "true" ]; then
         --insecure \
         --mixedform \
         "Configuration" 20 50 0 \
-        "ZFS Zpool Name:"   1 1 "$CONFIG_POOL"      1 18 26 80 0 \
-        "System Locale:"    2 1 "$CONFIG_LOCALE"    2 18 26 80 0 \
-        "System Timezone:"  3 1 "$CONFIG_TIMEZONE"  3 18 26 80 0 \
-        "Desktop Package:"  4 1 "$CONFIG_DESKTOP"   4 18 26 200 0 \
-        "HTTP Proxy:"       5 1 "$CONFIG_PROXY"     5 18 26 200 0 \
-        "Root Passwd:"      6 1 "$CONFIG_ROOT_PW"   6 18 26 16 1 \
-        "User Login:"       7 1 "$CONFIG_USER"      7 18 26 16 0 \
-        "User Passwd:"      8 1 "$CONFIG_USER_PW"   8 18 26 16 1 \
-        "User Full Name:"   9 1 "$CONFIG_USER_FN"   9 18 26 16 0 \
+        "Desktop Package:"  1 1 "$CONFIG_DESKTOP"   1 18 26 200 0 \
+        "Root Passwd:"      2 1 "$CONFIG_ROOT_PW"   2 18 26 16 1 \
+        "User Login:"       3 1 "$CONFIG_USER"      3 18 26 16 0 \
+        "User Passwd:"      4 1 "$CONFIG_USER_PW"   4 18 26 16 1 \
+        "User Full Name:"   5 1 "$CONFIG_USER_FN"   5 18 26 16 0 \
+        "System Locale:"    6 1 "$CONFIG_LOCALE"    6 18 26 80 0 \
+        "System Timezone:"  7 1 "$CONFIG_TIMEZONE"  7 18 26 80 0 \
+        "HTTP Proxy:"       8 1 "$CONFIG_PROXY"     8 18 26 200 0 \
+        "ZFS Zpool Name:"   9 1 "$CONFIG_POOL"      9 18 26 80 0 \
         2>"$tempfile"; then
 
         # assume the user wanted to cancel
@@ -32,13 +32,13 @@ if [ "$CONFIG_UNATTENDED" != "true" ]; then
         sed -n "${NR}p" "$tempfile"
     }
 
-    CONFIG_POOL=$(get_line 1)
-    CONFIG_LOCALE=$(get_line 2)
-    CONFIG_TIMEZONE=$(get_line 3)
-    CONFIG_DESKTOP=$(get_line 4)
-    CONFIG_PROXY=$(get_line 5)
-    CONFIG_ROOT_PW=$(get_line 6)
-    CONFIG_USER=$(get_line 7)
-    CONFIG_USER_PW=$(get_line 8)
-    CONFIG_USER_FN=$(get_line 9)
+    CONFIG_DESKTOP=$(get_line 1)
+    CONFIG_ROOT_PW=$(get_line 2)
+    CONFIG_USER=$(get_line 3)
+    CONFIG_USER_PW=$(get_line 4)
+    CONFIG_USER_FN=$(get_line 5)
+    CONFIG_LOCALE=$(get_line 6)
+    CONFIG_TIMEZONE=$(get_line 7)
+    CONFIG_PROXY=$(get_line 8)
+    CONFIG_POOL=$(get_line 9)
 fi
