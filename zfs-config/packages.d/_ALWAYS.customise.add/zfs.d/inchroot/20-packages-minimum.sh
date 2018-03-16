5a2f109e7fadaa36351479b2cf89b86ac46587ec
Hamish Coleman <hamish@zot.org>
2018-03-16 14:19:03 +0800

    Address EFS boot issues by ensuring that we are using all the created EFS partitions.  This also increases the resiliancy in disc failed situations

--- zfs-config/packages.d/_ALWAYS.customise.add/zfs.d/inchroot/20-packages-minimum.sh
+++ zfs-config/packages.d/_ALWAYS.customise.add/zfs.d/inchroot/20-packages-minimum.sh
@@ -5,3 +5,4 @@
 PACKAGES+=" linux-image-generic"        # A kernel to boot
 PACKAGES+=" zfs-initramfs"              # Infrastructure for booting ZFS
 PACKAGES+=" dosfstools grub-efi-amd64"  # Grub and its needed helper
+PACKAGES+=" mdadm"                      # tools to manage the ESP mirror
--- zfs-config/packages.d/_ALWAYS.customise.add/zfs.d/inchroot/90-grub-efi.sh
+++ zfs-config/packages.d/_ALWAYS.customise.add/zfs.d/inchroot/90-grub-efi.sh
@@ -7,23 +7,37 @@
 # figure out which partitions are ESP
 #PARTS=$(lsblk -r -o "name,parttype" |grep c12a7328-f81f-11d2-ba4b-00a0c93ec93b)
 
-# for now, just take the first part9
-PART=$(find /dev/disk/by-id/|grep -- -part9 |head -1)
+# A list of all ESP partitions
+# FIXME - we assume that part9 is always and only ESP
+PARTS=$(find /dev/disk/by-id/|grep -- -part9 ||true)
 
-# systems that don't have disk id labels (eg: a virtio disks)
-if [ -z "$PART" ]; then
-    PART=$(lsblk -e 2 -e 11 -o NAME -n -r -p |grep 9$ |head -1)
+# systems that don't have disk by-id labels (eg: virtio disks)
+if [ -z "$PARTS" ]; then
+    PARTS=$(lsblk -e 2 -e 11 -o NAME -n -r -p |grep 9$ ||true)
 fi
 
-if [ -z "$PART" ]; then
-    echo ERROR: could not find ESP partition
+if [ -z "$PARTS" ]; then
+    echo ERROR: could not find any ESP partitions
     false
 fi
-echo "Found ESP partition: $PART"
+echo "Found ESP partitions: $PARTS"
 
-wipefs -a "$PART"
-mkdosfs -F 32 -n ESP "$PART"
-UUID=$(blkid -s UUID -o value "$PART")
+for i in $PARTS; do
+    wipefs -a "$i"
+done
+
+PARTSNR=$(echo "$PARTS" | wc -w)
+FSDEV=/dev/md0
+
+# shellcheck disable=SC2086
+mdadm --create $FSDEV --metadata=0.90 --level=mirror \
+    --raid-devices="$PARTSNR" --force $PARTS
+# Note that the --force is here so that we can have a mirror with 1 drive in
+# it, which is useful in a test environment and provides a consistant setup
+# to production hardware
+
+mkdosfs -F 32 -n ESP "$FSDEV"
+UUID=$(blkid -s UUID -o value "$FSDEV")
 if [ -z "$UUID" ]; then
     echo ERROR: could not find UUID of ESP partition
     false
@@ -36,9 +50,3 @@ mount /boot/efi
 grub-install --target=x86_64-efi --efi-directory=/boot/efi \
     --bootloader-id=ubuntu --recheck --no-floppy
 umount /boot/efi
-
-# TODO
-# - need to duplicate this to all other ESP partitions
-# - thus should mount it with something other than the PARTUUID
-# - could benefit by creating a mirrorset..
-# - 
