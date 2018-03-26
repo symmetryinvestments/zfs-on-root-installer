62791f98b6141d7591c9ef4f6eb3b09f11ceabc2
Hamish Coleman <hamish@zot.org>
2018-03-26 16:53:01 +0800

    Do not create a mdadm with duplicate device nodes - that doesnt work

--- zfs-config/packages.d/_ALWAYS.customise.add/zfs.d/inchroot/90-grub-efi.sh
+++ zfs-config/packages.d/_ALWAYS.customise.add/zfs.d/inchroot/90-grub-efi.sh
@@ -9,19 +9,28 @@
 
 # A list of all ESP partitions
 # FIXME - we assume that part9 is always and only ESP
-PARTS=$(find /dev/disk/by-id/|grep -- -part9 ||true)
+PARTS_BYID=$(find /dev/disk/by-id/ |grep -- -part9 ||true)
 
 # systems that don't have disk by-id labels (eg: virtio disks)
-if [ -z "$PARTS" ]; then
-    PARTS=$(lsblk -e 2 -e 11 -o NAME -n -r -p |grep 9$ ||true)
+if [ -z "$PARTS_BYID" ]; then
+    PARTS_BYID=$(lsblk -e 2 -e 11 -o NAME -n -r -p |grep 9$ ||true)
 fi
 
-if [ -z "$PARTS" ]; then
+if [ -z "$PARTS_BYID" ]; then
     echo ERROR: could not find any ESP partitions
     false
 fi
+
+# shellcheck disable=SC2086
+# Make sure we have only detected one instance of each ESP partition
+PARTS=$(readlink -f $PARTS_BYID |sort |uniq)
+
 echo "Found ESP partitions: $PARTS"
 
+# Before trying to create anything, ensure that nothing is using the devices
+mdadm --stop --scan
+
+# and wipe any remaining superblocks
 for i in $PARTS; do
     wipefs -a "$i"
 done
@@ -47,6 +56,13 @@ echo "Found ESP partition UUID: $UUID"
 mkdir -p /boot/efi
 echo "UUID=$UUID /boot/efi vfat nofail,x-systemd.device-timeout=1 0 1" >> /etc/fstab
 mount /boot/efi
+# FIXME
+# - this outputs an error message about "-d needs a param"
+#   probably caused because grub cannot find the physical disk for our mdarray.
+#   Most of the time this should not cause any issues - since it is simply
+#   trying to set the default boot OS in the BIOS, however if we are dualbooting
+#   (unlikely) or have a complex disk environment (more likely) then this could
+#   hurt.
 grub-install --target=x86_64-efi --efi-directory=/boot/efi \
     --bootloader-id=ubuntu --recheck --no-floppy
 umount /boot/efi
