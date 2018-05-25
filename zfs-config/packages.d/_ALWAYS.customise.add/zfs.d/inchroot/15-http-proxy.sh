984b296342254b2b8e1bf7b173064caeaefe4516
Hamish Coleman <hamish@zot.org>
2018-05-25 11:05:57 +0800

    Add feature to carry any proxy config from the installer into the installed system apt config

--- zfs-config/packages.d/_ALWAYS.customise.add/zfs.d/inchroot/15-http-proxy.sh
+++ zfs-config/packages.d/_ALWAYS.customise.add/zfs.d/inchroot/15-http-proxy.sh
@@ -2,4 +2,7 @@
 # If we have a proxy in the installer, copy that config into the target
 #
 
-# TODO
+if [ -n "$http_proxy" ]; then
+    echo "Acquire::http::Proxy \"$http_proxy\";" >/etc/apt/apt.conf.d/proxy
+fi
+
