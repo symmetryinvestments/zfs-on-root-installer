d716844aff9e418c5e4deca41379c98af549255a
Hamish Coleman <hamish@zot.org>
2018-06-28 12:57:47 +0800

    Document some future thoughts about adding the rescue image inside the installed system

---
+++ zfs-config/packages.d/_ALWAYS.customise.add/zfs.d/30-rescue.sh
@@ -0,0 +1,17 @@
+#
+# Future improvement - allow the installed system to boot into the installer
+#
+
+# TODO
+# - if there is enough RAM
+# - and probably if not disabled by config
+# - and if the boot media is still around
+# - search for the boot media and mount it
+# - extract the kernel and initramfs
+# - place these somewhere well known
+
+# as part of the inchroot scripts
+# - if the kernel and initramfs are somewhere well known
+# - copy them into the right place on the EFS boot partition
+# - create a nsh script to allow booting into the rescue system
+
