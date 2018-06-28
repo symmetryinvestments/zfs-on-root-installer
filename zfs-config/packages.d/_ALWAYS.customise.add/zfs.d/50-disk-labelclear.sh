739f7d4fc96e3baef3522f1b551dc447d30637f1
Hamish Coleman <hamish@zot.org>
2018-06-28 13:34:20 +0800

    Wiping the disk labels can fail if the pool has already been discovered - try to avoid this situation by exporting any pools first

--- zfs-config/packages.d/_ALWAYS.customise.add/zfs.d/50-disk-labelclear.sh
+++ zfs-config/packages.d/_ALWAYS.customise.add/zfs.d/50-disk-labelclear.sh
@@ -3,6 +3,9 @@
 # with similar details
 #
 
+# If there are any pools already discovered, they could cause issues as well
+zpool export -af
+
 for dev in $(blkid -t "LABEL=$CONFIG_POOL" -o device); do
     echo "Found $dev with same label as our zpool - running labelclear"
     zpool labelclear -f "$dev"
