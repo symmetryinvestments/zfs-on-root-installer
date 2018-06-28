30d7d538b5cd73020a4ac894b26ccb2329c74387
Hamish Coleman <hamish@zot.org>
2018-06-28 13:35:17 +0800

    During the grub install, udev might find the EFS and try to auto assemble it, which causes problems - so turn that feature off

--- zfs-config/packages.d/_ALWAYS.customise.add/zfs.d/49-mdadm.sh
+++ zfs-config/packages.d/_ALWAYS.customise.add/zfs.d/49-mdadm.sh
@@ -11,4 +11,8 @@ mdadm --stop --scan
 if grep ^md /proc/mdstat; then
     echo ERROR: mdadm devices still exist even after trying to stop them
     false
+    exit
 fi
+
+# disable auto-assembly of any md devices
+echo "AUTO -all" >>/etc/mdadm/mdadm.conf
