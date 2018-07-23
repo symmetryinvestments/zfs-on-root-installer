feba151ee1e73a1a6922cebb36f80cfdcd05852e
Hamish Coleman <hamish@zot.org>
2018-07-23 11:47:40 +0100

    Handle bad locale strings with a fallback to en_US

--- zfs-config/packages.d/_ALWAYS.customise.add/zfs.d/inchroot/11-locale.sh
+++ zfs-config/packages.d/_ALWAYS.customise.add/zfs.d/inchroot/11-locale.sh
@@ -2,6 +2,16 @@
 # Configure the locale
 #
 
-locale-gen "$CONFIG_LOCALE"
+if ! locale-gen "$CONFIG_LOCALE"; then
+    # The user specified locale doesnt validate, use a hardcoded fallback.
+    echo "WARNING: The locale $CONFIG_LOCALE is invalid"
+
+    # The locale can easily be set once the system is installed..
+    CONFIG_LOCALE=en_US.UTF-8
+    echo "INFO: Using $CONFIG_LOCALE as a fallback locale"
+
+    locale-gen "$CONFIG_LOCALE"
+fi
+
 echo "LANG=\"$CONFIG_LOCALE\"" > /etc/default/locale
 
